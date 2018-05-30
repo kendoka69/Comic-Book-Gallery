9f45236f643f735779266f8ea02545e3575bbdb8
elizabeth hahn <kendoka69@mac.com>
2018-05-30 17:36:33 -0400

    Added comic book repository

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
@@ -27,7 +26,7 @@ namespace ComicBookGallery.Controllers
                 }
             };
 
-            return View(comicBook);
+            return View (comicBook);
         }
     }
 }
