f4b1fbaa456c0b4450c6bfd1a4fc589ff9dea87b
elizabeth hahn <kendoka69@mac.com>
2018-05-31 13:13:26 -0400

    Updated comic book controller to use repository.

--- ComicBookGallery/Data/ComicBookRepository.cs
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -11,9 +11,10 @@ namespace ComicBookGallery.Data
         {
             new ComicBook()
             {
-                SeriesTitle = "The Amazing Spider Man",
+                Id = 1,
+                SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Favorite = true,
                 Artists = new Artist[]
                 {
@@ -25,7 +26,8 @@ namespace ComicBookGallery.Data
                 }
             },
             new ComicBook()
-            {
+            {   
+                Id = 2,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
                 DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -40,7 +42,8 @@ namespace ComicBookGallery.Data
                 Favorite = false
             },
             new ComicBook()
-            {
+            {   
+                Id = 3,
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
                 DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
